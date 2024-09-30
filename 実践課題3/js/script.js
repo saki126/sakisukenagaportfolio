c64e72e8186e251183806f6c93fcd4ef8d16ad20
saki126 <skrnnnn126@gmail.com>
2024-10-01 03:25:27 +0900

    Add files via upload

---
+++ 実践課題3/js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".instagram .slide ul").slick({
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 1,
+        autoplaySpeed: 0,
+        cssEase: 'linear',
+        speed: 10000
+    });
+});
